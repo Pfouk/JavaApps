1e03b631871d9717c330a941095e602eb85d5987
leschinsky <ileschinsky@objectstyle.com>
2020-01-31 19:51:18 +0300

    refactored Month selector

---
+++ src/com/os/ileschinsky/fundamentals/maintask/MonthsValues.java
@@ -0,0 +1,5 @@
+package com.os.ileschinsky.fundamentals.maintask;
+
+public enum MonthsValues {
+    JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
+}
