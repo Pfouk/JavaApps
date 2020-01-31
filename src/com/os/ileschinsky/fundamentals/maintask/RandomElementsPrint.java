e4dbfcf0d05b4e92a56adf47b01806a104d783e1
leschinsky <ileschinsky@objectstyle.com>
2020-01-31 19:56:13 +0300

    refactored Random elements print

--- src/com/os/ileschinsky/fundamentals/maintask/RandomElementsPrint.java
+++ src/com/os/ileschinsky/fundamentals/maintask/RandomElementsPrint.java
@@ -5,25 +5,25 @@ import java.util.Scanner;
 
 public class RandomElementsPrint {
     public static void main(String[] args) {
-        int numberOfElements;
+        int enteredNumberOfElements;
         Scanner in = new Scanner(System.in);
         System.out.print("Enter number of elements: ");
         if (!in.hasNextInt()){
             throw new IllegalArgumentException("Number of elements should be int!");
         }
-        numberOfElements = in.nextInt();
-        if (numberOfElements < 0) {
+        enteredNumberOfElements = in.nextInt();
+        if (enteredNumberOfElements < 0) {
             throw new IllegalArgumentException("Number of arguments can't be negative");
         }
-        Random r = new Random();
-        Random prnt = new Random();
-        for (int i = 0; i < numberOfElements; i++) {
-            switch (prnt.nextInt(2)){
+        Random randomIntToPrint = new Random();
+        Random printInLineOrNextLine = new Random();
+        for (int i = 0; i < enteredNumberOfElements; i++) {
+            switch (printInLineOrNextLine.nextInt(2)){
                 case 0:
-                    System.out.println(r.nextInt());
+                    System.out.println(randomIntToPrint.nextInt());
                     break;
                 case 1:
-                    System.out.print(r.nextInt() + " ");
+                    System.out.print(randomIntToPrint.nextInt() + " ");
                     break;
                 default:
                     System.out.println("Invalid choice");
